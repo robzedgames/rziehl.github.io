e45bcf68b38b92c018fb5b57cbbcf6689e967c6b
Robert Ziehl <robertziehl@gmail.com>
2022-01-23 22:19:14 -0700

    Removed blog post about Playdate

--- src/components/bio.js
+++ src/components/bio.js
@@ -10,7 +10,6 @@ import * as React from "react"
 const Bio = () => {
   return (
     <div className="bio">
-      <p>And I believe that you are the sum of what you create.</p>
       <p>
         Before I die, I want to make a game, release an album and write a
         book.
